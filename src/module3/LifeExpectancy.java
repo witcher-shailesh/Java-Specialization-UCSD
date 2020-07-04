9245560402f8f165d0d971698e852e82cf8b83b8
shailesh gahlawat <shailesh.techcourse@gmail.com>
2020-07-04 21:32:28 +0530

    changes

---
+++ src/module3/LifeExpectancy.java
@@ -0,0 +1,15 @@
+package module3;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import processing.core.PApplet;
+
+public class LifeExpectancy extends PApplet{
+	
+	UnfoldingMap map;
+	public void setup() {
+		
+	}
+	public void draw() {
+		
+	}
+}
